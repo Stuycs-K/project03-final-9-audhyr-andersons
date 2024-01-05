1b67afc86020541f144df53660f891e36f100a5d
Riasat Audhy <raudhy40@stuy.edu>
2024-01-05 14:57:09 -0500

    started fill deck function

--- game.c
+++ game.c
@@ -9,3 +9,14 @@ check cards
 Shuffle deck
 */
 
+void fillDeck(struct card* deck){
+    deck = newCard();
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 13; j++){
+            if(i+j != 0){
+                
+            }
+        }
+    }
+}
