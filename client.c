37bb6074636fcf0605eed7c81cb2ff0876ca4acb
Riasat Audhy <raudhy40@stuy.edu>
2024-01-10 14:56:34 -0500

    started working on client a bit

--- client.c
+++ client.c
@@ -1,6 +1,27 @@
 #include "networking.h"
 
-int main(int argc, char*argv[]){
+int resOrNew(){
+    char buff[100];
+    printf("Would you like to start a new game (1) or resume old one (2)?");
+    fgets(buff, sizeof(buff), stdin);
+    if(strlen(buff) != 2){
+        printf("%ld\n", strlen(buff));
+        printf("Invalid Input!\n");
+        return resOrNew();
+    }
 
-    
+    int choice = 0;
+    choice = buff[0] - 48;
+    if(choice != 1 && choice != 2){
+        printf("%d\n", choice);
+        printf("Invalid Input!\n");
+        return resOrNew();
+    }
+
+    return choice;
+}
+
+int main(int argc, char*argv[]){
+    int c = resOrNew();
+    return  0;
 }
