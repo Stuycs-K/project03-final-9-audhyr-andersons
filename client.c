fe56db5e765249df8fefba822c4e463f1b49eecf
Sanderson40 <sanderson40@stuy.edu>
2024-01-14 11:37:01 -0500

    worked on client and server.c

--- client.c
+++ client.c
@@ -10,11 +10,16 @@ struct card * stringtostruct(char* string){
 struct card *readcards(int serverd, struct card ** deck){
     //struct card* temp = deck;
     for(int i = 0; i<3; i++){
-        
         char buff[24];
-        read(serverd,buff, 24);
-        
-        *deck =addAtEnd(*deck, stringtostruct(buff));
+        struct card * card2;
+        int status =read(serverd,card2, 24);
+        if(status ==0){
+            perror("Reading failed");
+        }
+        //printf("number of bytes read %d", status);
+        //struct card * card2 = stringtostruct(buff);
+        printnice(card2);
+        *deck =addAtEnd(*deck, card2);
     }
     
     return *deck;
@@ -56,15 +61,16 @@ int main(int argc, char*argv[]){
     write(serverd, &c, sizeof(int));
     struct card* deck;
     char buff[100];
-    //readcards(serverd, deck);
+
     while(1){
         deck=NULL;
         deck =readcards(serverd,&deck );
-        printnice(deck);
-        printf("Please enter which card to pick(1-3): ");
+        printnice(deck); 
+        
+         printf("Please enter which card to pick(1-3): ");
         fgets(buff, sizeof(buff), stdin);
-        int choice = 0;
-    choice = buff[0] - 48;
+       int choice = 0;
+     choice = buff[0] - 48;
         if(strlen(buff) != 2){
         printf("%ld\n", strlen(buff));
         printf("Invalid Input!\n");
@@ -85,30 +91,4 @@ int main(int argc, char*argv[]){
     }}
     return 0;
     }
-    // struct card * top=NULL;
-    // char buff[24];
-    //read(serverd, buff, 20);
-    //printf("%s\n", buff);
-    // read(serverd,buff, 24);
-    // top = stringtostruct(buff);
-
-    // struct card * temp;
-    // //segfaulting here
-    // read(serverd,buff, 24);
-    // top=addAtEnd(top, stringtostruct(buff));
-    // //printf("hi\n");
-    // read(serverd,buff, 24);
-    // addAtEnd(top, stringtostruct(buff));
-     //printnice(top);
-    // printnice(top);
     
-    // struct card * top;
-    // read(serverd,top, 24);
-    // struct card * temp;
-    // //segfaulting here
-    // read(serverd,temp, 24);
-    // //top=addAtEnd(top, temp);
-    // //printf("hi\n");
-    // read(serverd,temp, 24);
-    // //addAtEnd(top, temp);
-    // //printnice(top);
--- server.c
+++ server.c
@@ -7,114 +7,23 @@ char* structtostring(struct card * cards, char* buffer){
 }
 int playgame(int clientd, struct card ** serv, struct card ** clie){
   srand(time(NULL));
-    while((deckSize(*serv)!=0) || (deckSize(*clie)!=0) ){
+    while((deckSize(*serv)!=0) && (deckSize(*clie)!=0) ){
       int choice;
-      struct card * top = clie;
+      struct card ** top = clie;
       for(int i =0;i<3;i++){
-        ////printf("HI3\n");
         char buff[24];
-        write(clientd, structtostring(*clie, buff), 24);
+        write(clientd, *clie, 24);
+        //printf("String to be sent%s\n",buff);
         *clie = (*clie)->next;
       }
-
       read(clientd, &choice, sizeof(choice));
-      //printf("HI1\n");
       games(serv, 1,top, choice);
-      printf("HI2\n");
+
       
     }
 
 }
-// int gaming(){
-//   int listeningsociket = server_setup();
-//   int clientd= server_tcp_handshake(listeningsociket);
-// int val=0;
-//     char buff [100];
-//     //printf("Start a new game or resume old one:(1/2) "); 
-//   //fgets(buff,sizeof(buff),stdin);
-//   //doesn't work, trying to take user input and either start a new game or resume old game based on what they type
-//   //printf("%ld\n",strlen(buff));
-//   if(strlen(buff)==2){
-//     if(buff[0]=='1'){
-//       struct card* deck =genDeck();
-//       struct card * deck2;
-//       struct card * deck3;
-//       //printf("Newly generated deck\n");
-//       for(int i = 0; i < 15; i++){
-//         deck=shuffleDeck(deck);
-//       }
-//       //printnice(deck);
-//       //printf("\n\n------\n");
-//       deck2=splitdeck(&deck, &deck2);
-//   //printnice(deck);
-//       deck3=splitdeck2(deck,deck3);
-//       //printf("\n\n------\n");
-//      // printnice(deck2);
-//         //printf("\n\n------\n");
-//        //printnice(deck3);
-//        struct card* temp = deck3;
-       
-//        for(int i =0; i< 3; i++){
-//         char buff[24];//="hi from server";
-    
-    
-//         write(clientd, structtostring(temp, buff), 24);
-//         temp = temp->next;
-//        }//segfaulting}
-//     }
-//     else if(buff[0]=='2'){
-//       printf("Add resuming code here\n");
-//     }
-//     else{printf("Hey! That wasn't an option, please pick type 1 for a new game or 2 to resume\n");
-//       gaming();}
-//   }
-//   else{
-//       printf("Hey! Your answer is too long, please just pick 1 or 2\n");
-//       gaming();
-//     }}
-// int gaming(){
-//   int val=0;
-//   char buff [100];
-//   printf("Start a new game or resume old one:(1/2) "); 
-//   fgets(buff,sizeof(buff),stdin);
-//   //doesn't work, trying to take user input and either start a new game or resume old game based on what they type
-//   //printf("%ld\n",strlen(buff));
-//   if(strlen(buff)==2){
-//     if(buff[0]=='1'){
-//       struct card* deck =genDeck();
-//       struct card * deck2;
-//       struct card * deck3;
-//       //printf("Newly generated deck\n");
-//       for(int i = 0; i < 15; i++){
-//         deck=shuffleDeck(deck);
-//       }
-//       //printnice(deck);
-//       //printf("\n\n------\n");
-//       deck2=splitdeck(&deck, &deck2);
-//   //printnice(deck);
-//       deck3=splitdeck2(deck,deck3);
-//       //printf("\n\n------\n");
-//      // printnice(deck2);
-//         //printf("\n\n------\n");
-//        //printnice(deck3);
-//        struct card* temp = deck3;
-//        for(int i =0; i< 3; i++){
-//         write(clientd, temp, 24);
-//         temp = temp->next;
-//        }//segfaulting
-//     }
-//     else if(buff[0]=='2'){
-//       printf("Add resuming code here\n");
-//     }
-//     else{printf("Hey! That wasn't an option, please pick type 1 for a new game or 2 to resume\n");
-//       gaming();}
-//   }
-//   else{
-//       printf("Hey! Your answer is too long, please just pick 1 or 2\n");
-//       gaming();
-//     }
 
-// }
 int main(int argc, char*argv[]){
   int listeningsocket = server_setup();
 
@@ -132,17 +41,9 @@ int main(int argc, char*argv[]){
         deck=shuffleDeck(deck);
       }
       deck2=splitdeck(deck, deck2);
-    // }
       deck3=splitdeck2(deck,deck3);
-//       printnice(deck);
-//       printf("\n--------\n");
-//       printnice(deck2);
-// printf("\n--------\n");
-//       printnice(deck3);
-//       printf("\n--------\n");
-    playgame(clientd,&deck2,&deck3);
-
-  close(clientd);
+      playgame(clientd,&deck2,&deck3);
+      close(clientd);
   }
   return 0;
 }
