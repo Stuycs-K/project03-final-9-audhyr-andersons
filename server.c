395fe9ae82747e1ca4e85e26a7680f378af0b9a6
Riasat Audhy <riasat2audhy@gmail.com>
2024-01-14 10:36:17 -0500

    fixed wrong cards being written

--- server.c
+++ server.c
@@ -7,13 +7,19 @@ char* structtostring(struct card * cards, char* buffer){
 }
 int playgame(int clientd, struct card ** serv, struct card ** clie){
   srand(time(NULL));
+    printf("Starting client deck:\n");
+    printnice(*clie);
+    printf("---------\n");
     while((deckSize(*serv)!=0) || (deckSize(*clie)!=0) ){
       int choice;
+      struct card* iterator = *clie;
       for(int i =0;i<3;i++){
-        ////printf("HI3\n");
         char buff[24];
-        write(clientd, structtostring(*clie, buff), 24);
-        *clie = (*clie)->next;
+        printf("card wrote: ");
+        printcard(*clie);
+        printf("------\n");
+        write(clientd, structtostring(iterator, buff), 24);
+        iterator = iterator->next;
       }
       read(clientd, &choice, sizeof(choice));
       //printf("HI1\n");
@@ -23,96 +29,7 @@ int playgame(int clientd, struct card ** serv, struct card ** clie){
     }
 
 }
-// int gaming(){
-//   int listeningsociket = server_setup();
-//   int clientd= server_tcp_handshake(listeningsociket);
-// int val=0;
-//     char buff [100];
-//     //printf("Start a new game or resume old one:(1/2) "); 
-//   //fgets(buff,sizeof(buff),stdin);
-//   //doesn't work, trying to take user input and either start a new game or resume old game based on what they type
-//   //printf("%ld\n",strlen(buff));
-//   if(strlen(buff)==2){
-//     if(buff[0]=='1'){
-//       struct card* deck =genDeck();
-//       struct card * deck2;
-//       struct card * deck3;
-//       //printf("Newly generated deck\n");
-//       for(int i = 0; i < 15; i++){
-//         deck=shuffleDeck(deck);
-//       }
-//       //printnice(deck);
-//       //printf("\n\n------\n");
-//       deck2=splitdeck(&deck, &deck2);
-//   //printnice(deck);
-//       deck3=splitdeck2(deck,deck3);
-//       //printf("\n\n------\n");
-//      // printnice(deck2);
-//         //printf("\n\n------\n");
-//        //printnice(deck3);
-//        struct card* temp = deck3;
-       
-//        for(int i =0; i< 3; i++){
-//         char buff[24];//="hi from server";
-    
-    
-//         write(clientd, structtostring(temp, buff), 24);
-//         temp = temp->next;
-//        }//segfaulting}
-//     }
-//     else if(buff[0]=='2'){
-//       printf("Add resuming code here\n");
-//     }
-//     else{printf("Hey! That wasn't an option, please pick type 1 for a new game or 2 to resume\n");
-//       gaming();}
-//   }
-//   else{
-//       printf("Hey! Your answer is too long, please just pick 1 or 2\n");
-//       gaming();
-//     }}
-// int gaming(){
-//   int val=0;
-//   char buff [100];
-//   printf("Start a new game or resume old one:(1/2) "); 
-//   fgets(buff,sizeof(buff),stdin);
-//   //doesn't work, trying to take user input and either start a new game or resume old game based on what they type
-//   //printf("%ld\n",strlen(buff));
-//   if(strlen(buff)==2){
-//     if(buff[0]=='1'){
-//       struct card* deck =genDeck();
-//       struct card * deck2;
-//       struct card * deck3;
-//       //printf("Newly generated deck\n");
-//       for(int i = 0; i < 15; i++){
-//         deck=shuffleDeck(deck);
-//       }
-//       //printnice(deck);
-//       //printf("\n\n------\n");
-//       deck2=splitdeck(&deck, &deck2);
-//   //printnice(deck);
-//       deck3=splitdeck2(deck,deck3);
-//       //printf("\n\n------\n");
-//      // printnice(deck2);
-//         //printf("\n\n------\n");
-//        //printnice(deck3);
-//        struct card* temp = deck3;
-//        for(int i =0; i< 3; i++){
-//         write(clientd, temp, 24);
-//         temp = temp->next;
-//        }//segfaulting
-//     }
-//     else if(buff[0]=='2'){
-//       printf("Add resuming code here\n");
-//     }
-//     else{printf("Hey! That wasn't an option, please pick type 1 for a new game or 2 to resume\n");
-//       gaming();}
-//   }
-//   else{
-//       printf("Hey! Your answer is too long, please just pick 1 or 2\n");
-//       gaming();
-//     }
 
-// }
 int main(int argc, char*argv[]){
   int listeningsocket = server_setup();
 
